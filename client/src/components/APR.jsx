748cb7f1185efbdbf09713bdf8caa57b2b9af1f1
MacBook <alinauroze@hotmail.com>
2020-08-06 18:17:33 +0500

    feat: APR container added

---
+++ client/src/components/APR.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class APR extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div className = 'container'>
+                
+            </div>
+        )
+    }
+
+}
