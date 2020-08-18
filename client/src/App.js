c5e75e7ba4b767e75dea0bb616653b6ec89b4e1e
MacBook <alinauroze@hotmail.com>
2020-08-19 03:45:42 +0500

    feat: FE components added

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import SearchBar from './components/SearchBar'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import APR from './components/APR'
+import UserBar from './components/Userbar'
 
 const data1 = {
   isArchived : true,
@@ -22,7 +23,8 @@ class App extends React.Component{
   constructor (props) {
     super(props);
     this.state = {
-      view: "home"
+      view: "home",
+      login_view: "su",
     }
   }
 
@@ -30,21 +32,57 @@ class App extends React.Component{
     return (
       <>
         
-        <div style={{textAlign: "center", display: this.state.view == "login" ? "block" : "none"}}>
-          <Signup />
-          <Login />
+        <div style={{textAlign: "center", display: this.state.view === "login" ? "block" : "none"}}>
+          {
+            this.state.login_view == "li" ? <Login /> : <Signup />
+          }
         </div>
-        <div style={{display: this.state.view == "home" ? "block" : "none"}}>
+        <div style={{display: this.state.view === "home" ? "block" : "none"}}>
+          <UserBar />
           <SearchBar />
-          <APR 
-            data = {data1}
-          />
-          <APR 
-            data = {data2}
-          />
-          <APR 
-            data = {data2}
-          />
+          <div>
+            <div style = {{display: "inline-block", width: 250, height: 500, background: "grey", height: window.innerHeight - 120}} className = "filter-con">
+
+            </div>
+            <div style={{display: "inline-block", verticalAlign: "top", maxWidth: window.innerWidth - 300}}>
+              <APR 
+                data = {data1}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data1}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data1}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data1}
+              />
+              <APR 
+                data = {data2}
+              />
+              <APR 
+                data = {data2}
+              />
+            </div>
+          </div>
         </div>
       </>
     )
