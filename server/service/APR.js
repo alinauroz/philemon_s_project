e27aaefcd043f1a16cb7533a2ef5c18c9004cfe3
MacBook <alinauroze@hotmail.com>
2020-08-20 03:56:48 +0500

    feat: APR service added

---
+++ server/service/APR.js
@@ -0,0 +1,16 @@
+const integrations = {};
+
+integrations.APR = require("../integration/APR");
+
+const getAll = async () => {
+    try {
+        return integrations.APR.search();
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    getAll
+}
