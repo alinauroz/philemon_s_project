6be856be96fab10369a077d54358e3d624a797f0
MacBook <alinauroze@hotmail.com>
2020-08-22 22:44:22 +0500

    feat: submit form added

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const {toProcessCase} = require("../utils/format");
-
+const API = "http://localhost:3002/"
 const toCollect = ["APR/REQ NUM","FY APR","APR Object ID","APR Create DT","APR Create Date","APR Released to Routing","Req OBJ ID","REQ Create DT","REQ Create Date","REQ Released to Route","INCOMING MIPR","SCRM ASRS Attached","Requisitioner","Requisitioner Email","Office","Phone","Required Delivery","Acquisition Method","Rec Award Action Type","PURPOSE REQ","Type of IT Acquisition","REQ INCLUDE ICS","Cleared Contractor","Product Desc","Sole Source","Vendor 1","Vedor 2","Vendor 3","Suggested Vendor","Auth AMT","System","Prod Cat","Serv Cat","How","Where","FILE1_NAME","FILE1_LINK","FILE2_NAME","FILE2_LINK","FILE3_NAME","FILE3_LINK","DOCUMENT_CLASSIFICATION","CAVEAT","TERMS","DECLASSIFY_EVENT","DECLASSIFY_DATE","DECLASSIFY_CODE","DECLASSIFIED_BY","SECURITY_METHOD","First IT Approval","Multiple Review","CONTRACT PIN","MOD NUM","OBLIG DATE","ISSUE DATE","AWARD VENDOR","Line Tot","Commit Amt","Obligated"]
 
 export default class Form extends React.Component {
@@ -19,7 +19,13 @@ export default class Form extends React.Component {
     } 
 
     submit = () => {
-        console.log(this.state.store)
+        return await fetch (API + "APR", {
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(this.state.store);
+        })
     }
 
     render () {
@@ -28,7 +34,7 @@ export default class Form extends React.Component {
                 <p>Form</p>
                 {
                     toCollect.map(field => {
-                        return <input type = 'text' onClick = {this.onChange_} name = {toProcessCase(field)} placeholder = {field} className = 'form-input' />
+                        return <input type = 'text' onChange = {this.onChange_} name = {toProcessCase(field)} placeholder = {field} className = 'form-input' />
                     })
                 }
                 <input type = 'button' value = 'Submit' onClick = {this.submit} />
