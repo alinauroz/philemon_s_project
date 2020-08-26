bf8f90bb87ebb1241824bc2ea093de58de322760
MacBook <alinauroze@hotmail.com>
2020-08-26 22:21:41 +0500

    feat: upload at backend

--- server/api/APR.js
+++ server/api/APR.js
@@ -2,11 +2,33 @@ const express = require('express');
 const service = {};
 const bodyParser = require('body-parser');
 service.APR = require("../service/APR");
+const multer = require("multer");
 
 const Router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        req.filename = 'csv' + Date.now() + '.csv';
+        cb(null, req.filename)
+    }
+})
+
+const upload = multer({ storage });
+
 Router.use(bodyParser.urlencoded({ extended: false }));
 Router.use(bodyParser.json());
+
+Router.post("/csv", upload.single('csv'), async (req, res) => {
+    try {
+        res.send({name: req.filename});
+    }
+    catch (err) {
+        res.send({err})
+    }
+});
 
 Router.get("/", async (req, res) => {
     try {
