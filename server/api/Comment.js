6256917e6761ac2e3b2ead491e2e1007d8761c4f
MacBook <alinauroze@hotmail.com>
2020-09-17 01:37:06 +0500

    feat: endpoint added for fetching comments

--- server/api/Comment.js
+++ server/api/Comment.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const Router = express.Router();
 
+const service = {};
+service.user = require("../service/user");
+service.comment = require("../service/Comment")
+
 Router.post("/", async (req, res) => {
-    
+
 });
 
 Router.get("/:APR_NUM", async (req, res) => {
-
+    try {
+        let data = await service.comment.get(req.body.APR_NUM);
+        return res.send(data);
+    }
+    catch (err) {
+        console.log(err)
+        res.send({err})
+    }
 })
 
 module.exports = Router;
